26e83d9a5dd607467a0b299bb917a4a2cbf8ebb4
Vladimír Ulman <ulman@fi.muni.cz>
2024-04-13 08:39:32 +0200

    ADD: Fiji "facade" plugin got 'ztoxratio' and relevant checking

--- src/main/java/org/mastodon/ctc/baseline/AutonomousFullTrackerPlugin.java
+++ src/main/java/org/mastodon/ctc/baseline/AutonomousFullTrackerPlugin.java
@@ -10,8 +10,12 @@ public class AutonomousFullTrackerPlugin implements Command {
 	@Parameter
 	Context ctx;
 
-	@Parameter
+	private static final float NOT_TOUCHED_ZTOXRATIO = -0.369f;
+
+	@Parameter(description = "Either a path to .mastodon project file, or a template path such as folder/mask%04d.tif")
 	String path;
+	@Parameter(required = false, description = "Considered only for input TIFF series.")
+	float ztoxratio = NOT_TOUCHED_ZTOXRATIO;
 	@Parameter
 	int from;
 	@Parameter
@@ -22,6 +26,21 @@ public class AutonomousFullTrackerPlugin implements Command {
 
 	@Override
 	public void run() {
+		if (path.endsWith(".mastodon")) {
+			//mastodon project:
+			System.out.println("Btw, ignoring 'ztoxratio' as Mastodon project files come with own value for it.");
+		} else {
+			//tiff series:
+			if (ztoxratio == NOT_TOUCHED_ZTOXRATIO) {
+				System.out.println("Please, always provide the 'ztoxratio' for tiff series. Stopping now.");
+				return;
+			}
+			if (ztoxratio <= 0.f) {
+				System.out.println("Please, provide positive 'ztoxratio' for tiff series. Stopping now.");
+				return;
+			}
+		}
+
 		if (savepath == null || savepath.isEmpty() || savepath.startsWith("no"))
 			AutonomousFullTracker.main(new String[]{path,String.valueOf(from),String.valueOf(till)}, ctx);
 		else
